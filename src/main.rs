edf3984bf8dee6de50a84e07d8b8f9d0247633f7
Eddie Maldonado <eddie@lemald.org>
2023-11-26 13:58:05 -0500

    feat: serving of static content, add htmx

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,15 @@ use warp::Filter;
 
 #[tokio::main]
 async fn main() {
-    let hello = warp::path!("hello" / String).map(|name| format!("Hello, {}!", name));
+    let index = warp::get()
+        .and(warp::path::end())
+        .and(warp::fs::file("./static/index.html"));
 
-    warp::serve(hello).run(([127, 0, 0, 1], 3030)).await;
+    let static_content = warp::get()
+        .and(warp::path("static"))
+        .and(warp::fs::dir("./static"));
+
+    let routes = index.or(static_content);
+
+    warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
 }
