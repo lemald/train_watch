c33c3b428bd05388447e594da749b60be59743bc
Eddie Maldonado <eddie@lemald.org>
2023-11-27 21:10:10 -0500

    feat: loop calling API for Red Line vehicles

--- src/mbta_api.rs
+++ src/mbta_api.rs
@@ -3,11 +3,26 @@ use tokio::time::{sleep, Duration};
 use crate::CarNumberToVehicleId;
 
 pub async fn loop_poll_data(car_number_to_vehicle_id: &CarNumberToVehicleId) {
-    {
-        let mut car_number_to_vehicle_id = car_number_to_vehicle_id.lock().unwrap();
+    loop {
+        let _ = poll_data(&car_number_to_vehicle_id).await;
 
-        car_number_to_vehicle_id.insert("1848".to_string(), "123456".to_string());
+        {
+            let mut car_number_to_vehicle_id = car_number_to_vehicle_id.lock().unwrap();
+
+            car_number_to_vehicle_id.insert("1848".to_string(), "123456".to_string());
+        }
+
+        sleep(Duration::from_secs(5)).await;
     }
+}
 
-    sleep(Duration::from_secs(5)).await;
+async fn poll_data(
+    _car_number_to_vehicle_id: &CarNumberToVehicleId,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let _resp = reqwest::get("https://api-v3.mbta.com/vehicles?filter[route]=Red")
+        .await?
+        .text()
+        .await?;
+
+    Ok(())
 }
