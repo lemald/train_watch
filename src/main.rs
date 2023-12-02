8da6b49f2a8cba9c3737ccebeb5a6d591e876d54
Eddie Maldonado <eddie@lemald.org>
2023-12-02 15:29:03 -0500

    feat: store vehicle status, begin more work on routes

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,71 @@
 mod mbta_api;
-mod routes;
 
 use std::{
     collections::HashMap,
     sync::{Arc, Mutex},
 };
 
+use warp::Filter;
+
 use mbta_api::loop_poll_data;
-use routes::routes;
 
 type CarNumber = String;
 type VehicleId = String;
 type CarNumberToVehicleId = Arc<Mutex<HashMap<CarNumber, VehicleId>>>;
 
+#[derive(Debug)]
+enum Status {
+    StoppedAt,
+    InTransitTo,
+    IncomingAt,
+}
+
+#[derive(Debug)]
+struct VehicleStatus {
+    status: Status,
+    station_name: String,
+}
+
+type VehicleIdToVehicleStatus = Arc<Mutex<HashMap<VehicleId, VehicleStatus>>>;
+
 #[tokio::main]
 async fn main() {
     let car_number_to_vehicle_id: CarNumberToVehicleId = Arc::new(Mutex::new(HashMap::new()));
+    let vehicle_id_to_vehicle_status: VehicleIdToVehicleStatus =
+        Arc::new(Mutex::new(HashMap::new()));
 
     {
         let car_number_to_vehicle_id = car_number_to_vehicle_id.clone();
 
         tokio::spawn(async move {
-            loop_poll_data(&car_number_to_vehicle_id).await;
+            loop_poll_data(&car_number_to_vehicle_id, &vehicle_id_to_vehicle_status).await;
         });
     }
 
     warp::serve(routes()).run(([127, 0, 0, 1], 3030)).await;
+}
+
+fn routes() -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
+    let index = warp::get()
+        .and(warp::path::end())
+        .and(warp::fs::file("./static/index.html"));
+
+    let static_content = warp::get()
+        .and(warp::path("static"))
+        .and(warp::fs::dir("./static"));
+
+    let search = warp::post()
+        .and(warp::path("search"))
+        .and(warp::path::end())
+        .and(warp::body::content_length_limit(1024 * 16))
+        .and(warp::body::form())
+        .map(
+            |form: HashMap<String, String>| match HashMap::get(&form, "car-number") {
+                Some(number) => number.clone(),
+                None => "No car number given".to_string(),
+            },
+        )
+        .map(warp::reply::html);
+
+    index.or(static_content).or(search)
 }
--- src/mbta_api.rs
+++ src/mbta_api.rs
@@ -7,6 +7,9 @@ use serde_derive::{Deserialize, Serialize};
 use tokio::time::{sleep, Duration};
 
 use crate::CarNumberToVehicleId;
+use crate::Status;
+use crate::VehicleIdToVehicleStatus;
+use crate::VehicleStatus;
 
 #[derive(Serialize, Deserialize, Debug)]
 struct Carriage {
@@ -14,17 +17,30 @@ struct Carriage {
 }
 
 #[derive(Serialize, Deserialize, Debug)]
+struct Stop {
+    id: String,
+    name: String,
+}
+
+jsonapi_model!(Stop; "stop");
+
+#[derive(Serialize, Deserialize, Debug)]
 struct Vehicle {
     id: String,
     label: String,
+    current_status: String,
     carriages: Vec<Carriage>,
+    stop: Stop,
 }
 
-jsonapi_model!(Vehicle; "vehicle");
+jsonapi_model!(Vehicle; "vehicle"; has one stop);
 
-pub async fn loop_poll_data(car_number_to_vehicle_id: &CarNumberToVehicleId) {
+pub async fn loop_poll_data(
+    car_number_to_vehicle_id: &CarNumberToVehicleId,
+    vehicle_id_to_vehicle_status: &VehicleIdToVehicleStatus,
+) {
     loop {
-        let _ = poll_data(&car_number_to_vehicle_id).await;
+        let _ = poll_data(&car_number_to_vehicle_id, &vehicle_id_to_vehicle_status).await;
 
         sleep(Duration::from_secs(5)).await;
     }
@@ -32,18 +48,32 @@ pub async fn loop_poll_data(car_number_to_vehicle_id: &CarNumberToVehicleId) {
 
 async fn poll_data(
     car_number_to_vehicle_id: &CarNumberToVehicleId,
+    vehicle_id_to_vehicle_status: &VehicleIdToVehicleStatus,
 ) -> Result<(), Box<dyn std::error::Error>> {
-    let resp = reqwest::get("https://api-v3.mbta.com/vehicles?filter[route]=Red")
+    let resp = reqwest::get("https://api-v3.mbta.com/vehicles?filter[route]=Red&include=stop")
         .await?
         .json::<DocumentData>()
         .await?;
 
     if let Some(PrimaryData::Multiple(vehicles)) = resp.data {
         let mut car_number_to_vehicle_id = car_number_to_vehicle_id.lock().unwrap();
+        let mut vehicle_id_to_vehicle_status = vehicle_id_to_vehicle_status.lock().unwrap();
+
         let mut car_numbers_present = HashSet::new();
+        let mut vehicle_ids_present = HashSet::new();
 
         for vehicle in vehicles.iter() {
-            let vehicle = Vehicle::from_jsonapi_resource(&vehicle, &None)?;
+            let vehicle = Vehicle::from_jsonapi_resource(&vehicle, &resp.included)?;
+
+            vehicle_ids_present.insert(vehicle.id.clone());
+
+            vehicle_id_to_vehicle_status.insert(
+                vehicle.id.clone(),
+                VehicleStatus {
+                    status: current_status_to_status(&vehicle.current_status)?,
+                    station_name: vehicle.stop.name.clone(),
+                },
+            );
 
             for carriage in vehicle.carriages.iter() {
                 car_number_to_vehicle_id.insert(carriage.label.clone(), vehicle.id.clone());
@@ -51,10 +81,20 @@ async fn poll_data(
             }
         }
 
+        vehicle_id_to_vehicle_status.retain(|k, _| vehicle_ids_present.contains(k));
         car_number_to_vehicle_id.retain(|k, _| car_numbers_present.contains(k));
 
-        println!("{:#?}", car_number_to_vehicle_id);
+        println!("{:#?}", vehicle_id_to_vehicle_status);
     }
 
     Ok(())
+}
+
+fn current_status_to_status(current_status: &str) -> Result<Status, &str> {
+    match current_status {
+        "STOPPED_AT" => Ok(Status::StoppedAt),
+        "IN_TRANSIT_TO" => Ok(Status::InTransitTo),
+        "INCOMING_AT" => Ok(Status::IncomingAt),
+        _ => Err("Invalid status"),
+    }
 }
