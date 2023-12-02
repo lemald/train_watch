88ac18cb48df7ef10d300c074b62334f237c212b
Eddie Maldonado <eddie@lemald.org>
2023-12-02 11:00:30 -0500

    fix: get rid of dummy code that added sample vehicle to hashmap

--- src/mbta_api.rs
+++ src/mbta_api.rs
@@ -26,12 +26,6 @@ pub async fn loop_poll_data(car_number_to_vehicle_id: &CarNumberToVehicleId) {
     loop {
         let _ = poll_data(&car_number_to_vehicle_id).await;
 
-        {
-            let mut car_number_to_vehicle_id = car_number_to_vehicle_id.lock().unwrap();
-
-            car_number_to_vehicle_id.insert("1848".to_string(), "123456".to_string());
-        }
-
         sleep(Duration::from_secs(5)).await;
     }
 }
